dac2c8fd3aab7db2be375ea986df07e4d8cf2987
Garry Blight <garry.blight@guardian.co.uk>
2017-01-06 12:46:31 +0000

    Scrape page elements and generate navs

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,64 @@ import share from './lib/share'
 
 var shareFn = share('Interactive title', 'http://gu.com/p/URL', '#Interactive');
 
+var breakfastSections;
+
 export function init(el, context, config, mediator) {
     el.innerHTML = mainHTML.replace(/%assetPath%/g, config.assetPath);
 
-    reqwest({
-        url: 'http://ip.jsontest.com/',
-        type: 'json',
-        crossOrigin: true,
-        success: (resp) => el.querySelector('.test-msg').innerHTML = `Your IP address is ${resp.ip}`
-    });
+    // reqwest({
+    //     url: 'http://ip.jsontest.com/',
+    //     type: 'json',
+    //     crossOrigin: true,
+    //     success: (resp) => el.querySelector('.test-msg').innerHTML = `Your IP address is ${resp.ip}`
+    // });
+
+    buildView( el );
 
     [].slice.apply(el.querySelectorAll('.interactive-share')).forEach(shareEl => {
         var network = shareEl.getAttribute('data-network');
         shareEl.addEventListener('click',() => shareFn(network));
     });
 }
+
+function buildView( el ) {
+
+    var i, buttonTexts = [], innerText, htmlString = '', navs;
+
+    breakfastSections = document.getElementsByTagName("blockquote")
+
+    for(i = 0; i < breakfastSections.length; i++) {
+
+        innerText = breakfastSections[i].getElementsByTagName("h2")[0].innerHTML;
+        buttonTexts.push( innerText );
+
+    }
+
+    for(i = 0; i < buttonTexts.length; i++) {
+
+        htmlString += '<h3 data-ind="' + i + '">' + buttonTexts[i] + '</h3>';
+
+    }
+
+    navs = document.getElementById("nav-list");
+
+    navs.innerHTML = htmlString;
+
+    navs.addEventListener ("click", navClick, false);
+
+    function navClick(e) {
+        //alert(e.target);
+        i = e.target.getAttribute('data-ind');
+
+        if ( i !== null ) {
+
+            var element = breakfastSections[i];
+            var alignWithTop = true;
+            element.scrollIntoView(alignWithTop);
+
+        }
+}
+
+}
+
+
